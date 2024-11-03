0218e7d9f37cb27e66d0c5d7f0ef3d8ef078c923
890-pranav <pranavmani890@gmail.com>
2024-11-04 00:52:44 +0530

    Fix expense findall api

--- expenseTracker/src/main/java/com/Project/expenseTrackerapi/entity/Expense.java
+++ expenseTracker/src/main/java/com/Project/expenseTrackerapi/entity/Expense.java
@@ -14,6 +14,54 @@ import lombok.*;
 @Entity
 @Table(name = "tbl_expense")
 public class Expense {
+	public Expense() {
+		super();
+	}
+	public Expense(Long id, String name, String description, BigDecimal amount, String category, Date date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.amount = amount;
+		this.category = category;
+		this.date = date;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public BigDecimal getAmount() {
+		return amount;
+	}
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
